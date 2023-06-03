e48751bb4c815b5bdd5af066eb5c5d569cffe50d
Ali Tunç <tunchalikali@gmail.com>
2023-06-03 04:18:09 +0300

    complete the css task

--- config/gulp-tasks/css-taks.js
+++ config/gulp-tasks/css-taks.js
@@ -1,14 +1,25 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
+const cleanCSS = require("gulp-clean-css");
+const gulpIf = require("gulp-if");
+const autoprefixer = require("gulp-autoprefixer");
+
 function runCss(platform) {
   let cssFiles = "./src/styles/**/*.scss";
+  let outputDir = "./dist/styles/";
 
   if (platform) {
     cssFiles = `./src/styles/pages/${platform}/**/*.scss`;
+    outputDir = `./dist/styles/pages/${platform}/`;
   }
 
-  return gulp.src(cssFiles).pipe(sass()).pipe(gulp.dest("./dist/styles/"));
+  return gulp
+    .src(cssFiles)
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulpIf(process.env.NODE_ENV == "production", cleanCSS()))
+    .pipe(gulp.dest(outputDir));
 }
 
 module.exports = {
