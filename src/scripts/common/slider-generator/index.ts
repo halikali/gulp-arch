4597040c477ee3d9135cb86f838438174d71efbe
Ali Tunç <tunchalikali@gmail.com>
2023-06-18 03:37:27 +0300

    manage sliders from one file

--- src/scripts/common/slider-generator/index.ts
+++ src/scripts/common/slider-generator/index.ts
@@ -1,8 +1,48 @@
 import Swiper from '../../../vendors/swiper/js/swiper-bundle.esm.browser.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const swiper = new Swiper('.mySwiper', {
-  pagination: {
-    el: '.swiper-pagination',
+interface ISlider {
+  selector: string;
+  options: object;
+}
+
+const sliders: ISlider[] = [
+  {
+    selector: '.mySwiper',
+    options: {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    },
   },
-});
+  {
+    selector: '.mySwiper2',
+    options: {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    },
+  },
+  {
+    selector: '.mySwiper3',
+    options: {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      loop: true,
+    },
+  },
+];
+
+const initializeSliders = () => {
+  const currentSliders = sliders.filter(({ selector }) =>
+    document.querySelector(selector),
+  );
+
+  currentSliders.forEach(({ selector, options }) => {
+    console.log(`${selector} seçicisine sahip slider aktifleştirildi.`);
+    new Swiper(selector, options);
+  });
+};
+
+initializeSliders();
